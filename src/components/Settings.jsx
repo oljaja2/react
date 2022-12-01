832822767e46237d9aeba0aa5f127dcd926cf183
OlgaSt <oljaja2@gmail.com>
2022-12-01 12:23:12 +0300

    settings first

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,25 @@
-const Settings = () => {
-  return <h2>Страница с настройками</h2>;
-};
+import { useState } from "react";
+
+function Settings() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="row">
+      <div className="col-6">
+        <p>Клики: {count}</p>
+      </div>
+      <div className="col-6">
+        <button
+          className="btn btn-warning"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          нажимай тут
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default Settings;
